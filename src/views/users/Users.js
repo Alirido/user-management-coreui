a85eed84d3cb8a1e569597370a65e2d14ecf6e88
Ali Rido <mohalirido@gmail.com>
2021-02-13 09:01:14 +0700

    Create user list and add user

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,86 +1,141 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import {
   CBadge,
+  CButton,
   CCard,
   CCardBody,
   CCardHeader,
   CCol,
+  CCollapse,
   CDataTable,
   CRow,
-  CPagination
-} from '@coreui/react'
+} from "@coreui/react";
 
-import usersData from './UsersData'
+import usersData from "./UsersData";
+import CIcon from "@coreui/icons-react";
 
-const getBadge = status => {
+const fields = [
+  { key: "name", _style: { width: "40%" } },
+  "registered",
+  { key: "role", _style: { width: "20%" } },
+  { key: "status", _style: { width: "20%" } },
+  {
+    key: "show_details",
+    label: "",
+    _style: { width: "1%" },
+    sorter: false,
+    filter: false,
+  },
+];
+
+const getBadge = (status) => {
   switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
+    case "Active":
+      return "success";
+    case "Inactive":
+      return "secondary";
+    case "Pending":
+      return "warning";
+    case "Banned":
+      return "danger";
+    default:
+      return "primary";
   }
-}
+};
 
 const Users = () => {
-  const history = useHistory()
-  const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
-  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
-  const [page, setPage] = useState(currentPage)
+  const [details, setDetails] = useState([]);
 
-  const pageChange = newPage => {
-    currentPage !== newPage && history.push(`/users?page=${newPage}`)
-  }
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index);
+    let newDetails = details.slice();
+    if (position !== -1) {
+      newDetails.splice(position, 1);
+    } else {
+      newDetails = [...details, index];
+    }
+    setDetails(newDetails);
+  };
 
-  useEffect(() => {
-    currentPage !== page && setPage(currentPage)
-  }, [currentPage, page])
+  const history = useHistory();
 
   return (
     <CRow>
-      <CCol xl={6}>
+      <CCol xl={10}>
         <CCard>
           <CCardHeader>
-            Users
-            <small className="text-muted"> example</small>
+            <strong>User List</strong>
           </CCardHeader>
           <CCardBody>
-          <CDataTable
-            items={usersData}
-            fields={[
-              { key: 'name', _classes: 'font-weight-bold' },
-              'registered', 'role', 'status'
-            ]}
-            hover
-            striped
-            itemsPerPage={5}
-            activePage={page}
-            clickableRows
-            onRowClick={(item) => history.push(`/users/${item.id}`)}
-            scopedSlots = {{
-              'status':
-                (item)=>(
+            <CDataTable
+              items={usersData}
+              fields={fields}
+              tableFilter
+              hover
+              pagination
+              itemsPerPageSelect
+              itemsPerPage={5}
+              sorter
+              pagination
+              scopedSlots={{
+                status: (item) => (
                   <td>
-                    <CBadge color={getBadge(item.status)}>
-                      {item.status}
-                    </CBadge>
+                    <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
                   </td>
-                )
-            }}
-          />
-          <CPagination
-            activePage={page}
-            onActivePageChange={pageChange}
-            pages={5}
-            doubleArrows={false} 
-            align="center"
-          />
+                ),
+                show_details: (item, index) => {
+                  return (
+                    <td className="py-2">
+                      <CButton
+                        color="primary"
+                        variant="outline"
+                        shape="square"
+                        size="sm"
+                        onClick={() => {
+                          toggleDetails(index);
+                        }}
+                      >
+                        {details.includes(index) ? "Hide" : "Actions"}
+                      </CButton>
+                    </td>
+                  );
+                },
+                details: (item, index) => {
+                  return (
+                    <CCollapse show={details.includes(index)}>
+                      <CCardBody>
+                        <h4>{item.username}</h4>
+                        <p className="text-muted">User with ID: {item.id}</p>
+                        <CButton
+                          size="sm"
+                          color="primary"
+                          onClick={() => history.push(`/users/${item.id}`)}
+                        >
+                          Show
+                        </CButton>
+                        <CButton size="sm" color="info" className="ml-1">
+                          Edit
+                        </CButton>
+                        <CButton size="sm" color="danger" className="ml-1">
+                          Delete
+                        </CButton>
+                      </CCardBody>
+                    </CCollapse>
+                  );
+                },
+              }}
+            />
           </CCardBody>
         </CCard>
       </CCol>
+      <CCol xl={2}>
+        <CButton block color="success" to="/users/create">
+          <CIcon size="sm" name="cil-user-follow" /> Add User
+        </CButton>
+      </CCol>
     </CRow>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
