85e452dd38521afcc29f83febf895cbf4a711e62
Ali Rido <mohalirido@gmail.com>
2021-02-14 22:51:59 +0700

    Add user edit component

--- src/views/users/UserEdit.js
+++ src/views/users/UserEdit.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, useLocation } from "react-router-dom";
 import {
-  CBadge,
   CButton,
   CCard,
   CCardBody,
   CCardHeader,
+  CCardFooter,
   CCol,
-  CDataTable,
+  CForm,
+  CFormGroup,
+  CFormText,
+  CInput,
+  CLabel,
   CRow,
-  CPagination,
 } from "@coreui/react";
 
-import usersData from "./UsersData";
 import CIcon from "@coreui/icons-react";
 
 const CreateUser = () => {
@@ -31,12 +32,75 @@ const CreateUser = () => {
 
   return (
     <CRow>
-      <CCol xl={10}>
+      <CCol xl={8}>
         <CCard>
           <CCardHeader>
-            <strong>Edit User</strong>
+            User Edit
+            <small> Form</small>
           </CCardHeader>
-          <CCardBody>Testing</CCardBody>
+          <CCardBody>
+            <CForm action="" method="post" className="form-horizontal">
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="hf-username">Username</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    type="username"
+                    id="hf-username"
+                    name="hf-username"
+                    placeholder="Enter Username..."
+                    autoComplete="current-username"
+                  />
+                  <CFormText className="help-block">
+                    Please enter your username
+                  </CFormText>
+                </CCol>
+              </CFormGroup>
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="hf-name">Name</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    type="name"
+                    id="hf-name"
+                    name="hf-name"
+                    placeholder="Enter Name..."
+                    autoComplete="current-name"
+                  />
+                  <CFormText className="help-block">
+                    Please enter your name
+                  </CFormText>
+                </CCol>
+              </CFormGroup>
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="hf-password">Password</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    type="password"
+                    id="hf-password"
+                    name="hf-password"
+                    placeholder="Enter Password..."
+                    autoComplete="current-password"
+                  />
+                  <CFormText className="help-block">
+                    Please enter your password
+                  </CFormText>
+                </CCol>
+              </CFormGroup>
+            </CForm>
+          </CCardBody>
+          <CCardFooter>
+            <CButton type="submit" size="sm" color="primary">
+              <CIcon name="cil-scrubber" /> Submit
+            </CButton>{" "}
+            <CButton type="reset" size="sm" color="danger">
+              <CIcon name="cil-ban" /> Reset
+            </CButton>
+          </CCardFooter>
         </CCard>
       </CCol>
     </CRow>
